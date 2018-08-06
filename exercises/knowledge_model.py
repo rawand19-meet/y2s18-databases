b71ce3048a066008efafd1f5dbf0d8d3cfb2dbbb
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 14:13:05 +0300

    yes

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -5,7 +5,19 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Knowledge(Base):
+class Animal(Base):
+    __tablename__'animal'
+    ID= Column(Integer,primary_key = True)
+    Giraffe_Information= Column(String)
+    Giraffes_and_humans = Column(String)
+    Types_of_Giraffes = Column(String)
+    def __repr__(self):
+            return("Animal Giraffe_Information:{}\n"
+	 		   "Animal Giraffes_and_humans:{}\n"
+	 		   "Animal Types_of_Giraffes:{}\n"	).format(
+	 		   self.Giraffe_Information , self.Giraffes_and_humans,self.Giraffes_and_humans)
+
+
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -13,5 +25,3 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
-
-	pass
